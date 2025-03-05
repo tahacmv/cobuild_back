ec1f171af0488a334b64fec2285271bde011b634
tahacmv <tmmotassimahmedtaha@gmail.com>
2025-03-05 09:38:47 +0100

    initial commit

---
+++ src/main/java/unice/miage/numres/cobuild/CobuildApplication.java
@@ -0,0 +1,13 @@
+package unice.miage.numres.cobuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobuildApplication.class, args);
+	}
+
+}
