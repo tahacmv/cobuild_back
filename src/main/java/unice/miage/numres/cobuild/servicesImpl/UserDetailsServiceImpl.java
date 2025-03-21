726d3204064170003860fa55bbf5429f2742d6cf
tahacmv <tmmotassimahmedtaha@gmail.com>
2025-03-21 02:47:04 +0100

    fixed user detail service

--- src/main/java/unice/miage/numres/cobuild/servicesImpl/UserDetailsServiceImpl.java
+++ src/main/java/unice/miage/numres/cobuild/servicesImpl/UserDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package unice.miage.numres.cobuild.servicesImpl;
 
 import lombok.RequiredArgsConstructor;
-import unice.miage.numres.cobuild.model.User;
+import unice.miage.numres.cobuild.model.Utilisateur;
 import unice.miage.numres.cobuild.repository.UserRepository;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,8 +19,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         @Override
         public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                User user = userRepository.findByUsername(username)
-                                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+                Utilisateur user = userRepository.findByUsername(username)
+                                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur not found: " + username));
 
                 return org.springframework.security.core.userdetails.User
                                 .withUsername(user.getUsername())
